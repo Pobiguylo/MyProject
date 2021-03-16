80d011c8bd496f05661265a5ed5151c3f45badf0
Pobiguylo <epobigaylo@mail.ru>
2021-03-16 17:59:54 +0300

    Its begin

---
+++ app/src/main/java/com/example/knead_yourself/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.knead_yourself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_main);
+    }
+}
