532ab4e2917d9d3b3f8e073ba026d42126725b3c
Pobiguylo <epobigaylo@mail.ru>
2021-04-05 18:46:32 +0300

    DataBase2

--- app/src/main/java/com/example/knead_yourself/DataBase.java
+++ app/src/main/java/com/example/knead_yourself/DataBase.java
@@ -7,85 +7,86 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
-public class DataBase  {
-    private static final String DATABASE_NAME = "simple.db";
+public class DataBase extends  SQLiteOpenHelper  {
+    private static final String DATABASE_NAME = "trainings.db";
     private static final int DATABASE_VERSION = 1;
     public static final String TABLE_NAME = "TRAININGS";
 
     private static final String COLUMN_ID = "id";
     public static final String COLUMN_NAME_TRAINING = "NAME_TRAINING";
-    public static final String COLUMN_EXERCISE_NAME = "EXERCISE_NAME";
-    public static final String COLUMN_EXERCISE_DESCRIPTION = "EXERCISE_DESCRIPTION";
-
-    private static final int NUM_COLUMN_ID = 0;
-    private static final int NUM_COLUMN_NAME_TRAINING = 1;
-    private static final int NUM_COLUMN_EXERCISE_NAME = 2;
-    private static final int NUM_COLUMN_EXERCISE_DESCRIPTION = 3;
-
-
-    private SQLiteDatabase mDataBase;
-
-    public DataBase(Context context) {
-        OpenHelper mOpenHelper = new OpenHelper(context);
-        mDataBase = mOpenHelper.getWritableDatabase();
+    public static final String COLUMN_EXERCISE_NAME1 = "EXERCISE_NAME1";
+    public static final String COLUMN_EXERCISE_DESCRIPTION1 = "EXERCISE_DESCRIPTION1";
+    public static final String COLUMN_EXERCISE_NAME2 = "EXERCISE_NAME2";
+    public static final String COLUMN_EXERCISE_DESCRIPTION2 = "EXERCISE_DESCRIPTION2";
+    public static final String COLUMN_EXERCISE_NAME3 = "EXERCISE_NAME3";
+    public static final String COLUMN_EXERCISE_DESCRIPTION3 = "EXERCISE_DESCRIPTION3";
+    public static final String COLUMN_EXERCISE_NAME4 = "EXERCISE_NAME4";
+    public static final String COLUMN_EXERCISE_DESCRIPTION4 = "EXERCISE_DESCRIPTION4";
+    public static final String COLUMN_EXERCISE_NAME5 = "EXERCISE_NAME5";
+    public static final String COLUMN_EXERCISE_DESCRIPTION5 = "EXERCISE_DESCRIPTION5";
+    public static final String COLUMN_EXERCISE_NAME6 = "EXERCISE_NAME6";
+    public static final String COLUMN_EXERCISE_DESCRIPTION6 = "EXERCISE_DESCRIPTION6";
+    public static final String COLUMN_EXERCISE_NAME7 = "EXERCISE_NAME7";
+    public static final String COLUMN_EXERCISE_DESCRIPTION7 = "EXERCISE_DESCRIPTION7";
+    public static final String COLUMN_EXERCISE_NAME8 = "EXERCISE_NAME8";
+    public static final String COLUMN_EXERCISE_DESCRIPTION8 = "EXERCISE_DESCRIPTION8";
+    public static final String COLUMN_EXERCISE_NAME9 = "EXERCISE_NAME9";
+    public static final String COLUMN_EXERCISE_DESCRIPTION9 = "EXERCISE_DESCRIPTION9";
+    public static final String COLUMN_EXERCISE_NAME10 = "EXERCISE_NAME10";
+    public static final String COLUMN_EXERCISE_DESCRIPTION10 = "EXERCISE_DESCRIPTION10";
+    DataBase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = ("CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME_TRAINING + " TEXT, " +
+                COLUMN_EXERCISE_NAME1 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION1 + " TEXT, "+
+                COLUMN_EXERCISE_NAME2 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION2 + " TEXT, "+
+                COLUMN_EXERCISE_NAME3 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION3 + " TEXT, "+
+                COLUMN_EXERCISE_NAME4 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION4 + " TEXT, "+
+                COLUMN_EXERCISE_NAME5 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION5 + " TEXT, "+
+                COLUMN_EXERCISE_NAME6 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION6 + " TEXT, "+
+                COLUMN_EXERCISE_NAME7 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION7 + " TEXT, "+
+                COLUMN_EXERCISE_NAME8 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION8 + " TEXT, "+
+                COLUMN_EXERCISE_NAME9 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION9 + " TEXT, "+
+                COLUMN_EXERCISE_NAME10 + " TEXT, " +
+                COLUMN_EXERCISE_DESCRIPTION10 + " TEXT);");
+        db.execSQL(query);
+        Trainings tr1 = new Trainings(1,"Head","ex1","dec1","ex2","dec2","ex3","dec3","ex4","dec4","ex5","dec5",null,null,null,null,null,null,null,null,null,null);
+        db.execSQL("INSERT INTO "+ TABLE_NAME +" (" + COLUMN_NAME_TRAINING
+                + ", " + COLUMN_EXERCISE_NAME1 +", "+COLUMN_EXERCISE_DESCRIPTION1+", "
+                + COLUMN_EXERCISE_NAME2 +", "+COLUMN_EXERCISE_DESCRIPTION3+", "
+                + COLUMN_EXERCISE_NAME3 +", "+COLUMN_EXERCISE_DESCRIPTION3+", "
+                + COLUMN_EXERCISE_NAME4 +", "+COLUMN_EXERCISE_DESCRIPTION4+", "
+                + COLUMN_EXERCISE_NAME5 +", "+COLUMN_EXERCISE_DESCRIPTION5+", "
+                + COLUMN_EXERCISE_NAME6 +", "+COLUMN_EXERCISE_DESCRIPTION6+", "
+                + COLUMN_EXERCISE_NAME7 +", "+COLUMN_EXERCISE_DESCRIPTION7+", "
+                + COLUMN_EXERCISE_NAME8 +", "+COLUMN_EXERCISE_DESCRIPTION8+", "
+                + COLUMN_EXERCISE_NAME9 +", "+COLUMN_EXERCISE_DESCRIPTION9+", "
+                + COLUMN_EXERCISE_NAME10 +", "+COLUMN_EXERCISE_DESCRIPTION10+") VALUES (tr1.getName,tr1.getExercise_name1,tr1.getExercise_description1,tr1.getExercise_name2,tr1.getExercise_description2,tr1.getExercise_name3,tr1.getExercise_description3,tr1.getExercise_name4,tr1.getExercise_description4,tr1.getExercise_name5,tr1.getExercise_description5,tr1.getExercise_name6,tr1.getExercise_description6,tr1.getExercise_name7,tr1.getExercise_description7,tr1.getExercise_name8,tr1.getExercise_description8,tr1.getExercise_name9,tr1.getExercise_description9,tr1.getExercise_name10,tr1.getExercise_description10);");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
-    public long insert(String name,String exercise_name,String exercise_description) {
-        ContentValues cv=new ContentValues();
-        cv.put(COLUMN_NAME_TRAINING, name);
-        cv.put(COLUMN_EXERCISE_NAME, exercise_name);
-        cv.put(COLUMN_EXERCISE_DESCRIPTION, exercise_description);
-        return mDataBase.insert(TABLE_NAME, null, cv);
-    }
-
-    public int update(Trainings md) {
-        ContentValues cv=new ContentValues();
-        cv.put(COLUMN_NAME_TRAINING, md.getName());
-        cv.put(COLUMN_EXERCISE_NAME, md.getExercise_name());
-        cv.put(COLUMN_EXERCISE_DESCRIPTION, md.getExercise_description());
-        return mDataBase.update(TABLE_NAME, cv, COLUMN_ID + " = ?",new String[] { String.valueOf(md.getId())});
-    }
-
-    public void deleteAll() {
-        mDataBase.delete(TABLE_NAME, null, null);
-    }
-
-    public void delete(long id) {
-        mDataBase.delete(TABLE_NAME, COLUMN_ID + " = ?", new String[] { String.valueOf(id) });
-    }
-
-    public Trainings select(long id) {
-        Cursor mCursor = mDataBase.query(TABLE_NAME, null, COLUMN_ID + " = ?", new String[]{String.valueOf(id)}, null, null, null);
-
-        mCursor.moveToFirst();
-        String name = mCursor.getString(NUM_COLUMN_NAME_TRAINING);
-        String exercise_name = mCursor.getString(NUM_COLUMN_EXERCISE_NAME);
-        String exercise_description = mCursor.getString(NUM_COLUMN_EXERCISE_DESCRIPTION);
-        return new Trainings(id, name, exercise_name, exercise_description);
     }
 
 
 
 
-    private class OpenHelper extends SQLiteOpenHelper {
-        OpenHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            String query = "CREATE TABLE " + TABLE_NAME + " (" +
-                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COLUMN_NAME_TRAINING + " TEXT, " +
-                    COLUMN_EXERCISE_NAME + " TEXT, " +
-                    COLUMN_EXERCISE_DESCRIPTION + " TEXT);";
-            db.execSQL(query);
-        }
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-            onCreate(db);
-        }
-    }
-}
+
 
--- app/src/main/java/com/example/knead_yourself/ListActivity.java
+++ app/src/main/java/com/example/knead_yourself/ListActivity.java
@@ -2,18 +2,41 @@ package com.example.knead_yourself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class ListActivity extends AppCompatActivity {
     ListView listView;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
+    DataBase dataBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         this.listView = findViewById(R.id.listview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1);
+         dataBase = new DataBase(getApplicationContext());
     }
+
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = dataBase.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        userCursor =  db.rawQuery("select * from "+ DataBase.TABLE_NAME, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DataBase.COLUMN_NAME_TRAINING};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1}, 0);
+        listView.setAdapter(userAdapter);
+    }
+
 }
