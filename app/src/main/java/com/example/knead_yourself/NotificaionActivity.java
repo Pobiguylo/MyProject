c2f720419f624035eea10d8bd68ac8034b16ae41
Pobiguylo <epobigaylo@mail.ru>
2021-05-31 14:54:00 +0300

    Перерисовка и уведомления робят но без времени

--- app/src/main/java/com/example/knead_yourself/NotificaionActivity.java
+++ app/src/main/java/com/example/knead_yourself/NotificaionActivity.java
@@ -6,11 +6,13 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -21,12 +23,17 @@ public class NotificaionActivity extends AppCompatActivity {
     EditText time;
     private static final int NOTIFY_ID = 1;
     private static String CHANNEL_ID = "Cat channel";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notificaion);
-        this.aSwitch = findViewById(R.id.switch1);
-        this.time = findViewById(R.id.time);
+
+    public static void createChannelIfNeeded(NotificationManager manager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public  void Not(){
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
         Intent notificationIntent = new Intent(NotificaionActivity.this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(NotificaionActivity.this,
                 0, notificationIntent,
@@ -34,19 +41,31 @@ public class NotificaionActivity extends AppCompatActivity {
 
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(NotificaionActivity.this, CHANNEL_ID)
+                        .setAutoCancel(false)
                         .setSmallIcon(android.R.drawable.sym_def_app_icon)
                         .setContentTitle("Пора работать")
                         .setContentText("Пора покормить кота")
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                         .setContentIntent(contentIntent);
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(NotificaionActivity.this);
+        createChannelIfNeeded(notificationManager);
         notificationManager.notify(NOTIFY_ID, builder.build());
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificaion);
+        this.aSwitch = findViewById(R.id.switch1);
+        this.time = findViewById(R.id.time);
+
         if (aSwitch != null) {
             aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
+                    if(aSwitch.isChecked()){
+                        Not();
+                    }
                 }
             });
         }
